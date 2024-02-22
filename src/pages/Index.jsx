c649adde667b2d1b1b8973fb22067c30885ce35b
alicexplore <alicebergonhe@gmail.com>
2024-02-23 00:36:44 +0100

    apply framer motion on logo

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { motion } from 'framer-motion'
+import { fadeIn } from '../variants'
 
 export default function EnterCode() {
   const [combination, setCombination] = useState(['2', '3', '8', '4', '9']);
@@ -24,6 +26,21 @@ export default function EnterCode() {
   return (
     <div className='flex justify-center items-center bg-[#222] h-[100vh]'>
       <div className="text-center">
+            <motion.div
+                variants={fadeIn('right', 1)}
+                initial='hidden'
+                whileInView={'show'}
+                viewport={{ once: true, amount: 0}}
+                className='fixed top-10 left-10 grid grid-cols-1 text-left gap-y-2'>
+                    <img src="/logo/logo-portfolio.svg" alt="" width={123}/>
+                    <motion.div
+                        variants={fadeIn('up', 2)}
+                        initial='hidden'
+                        whileInView={'show'}
+                        viewport={{ once: true, amount: 0}}>
+                        <h3 className="text-[#e9e9e9] tracking-wide font-apercu font-thin text-xs">Portfolio coming soon</h3>
+                    </motion.div>
+            </motion.div>
         <h1 className="fixed top-40 left-200 tracking-wide font-apercu text-[#222]">Enter with {correctCombination.join('')}</h1>
         <button className="fixed mt-4 px-4 py-2 top-10 right-2O tracking-wide font-thin font-apercu text-[#222] text-sm transition duration-700 ease-in-out hover:text-white">Not that simple, but remember, <br />your mouse/cursor knows everything.</button>
         <div className="flex justify-center">
