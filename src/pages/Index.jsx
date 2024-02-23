bd742390095bdc29e3dcf9b9db04e38f3080fda9
alicexplore <alicebergonhe@gmail.com>
2024-02-23 05:22:18 +0100

    add clue component

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Clue from "../components/Clue";
 import { motion } from 'framer-motion'
 import { fadeIn } from '../variants'
 
@@ -26,23 +27,17 @@ export default function EnterCode() {
   return (
     <div className='flex justify-center items-center bg-[#222] h-[100vh]'>
       <div className="text-center">
-            <motion.div
-                variants={fadeIn('right', 1)}
+        <div className="fixed top-5 right-5 z-20">
+            <Clue />
+        </div>
+        <h1 className="fixed top-40 left-200 tracking-wide font-apercu text-[#222]">Enter with {correctCombination.join('')}</h1>
+        <button className="fixed mt-4 px-4 py-2 top-10 right-2O tracking-wide font-thin font-apercu text-[#222] text-sm transition duration-700 ease-in-out hover:text-white">Not here.</button>
+        <button className="fixed px-4 py-2 bottom-10 left-20 tracking-wide font-thin font-apercu text-[#222] text-sm transition duration-700 ease-in-out hover:text-white">Not here, not that simple.</button>
+        <motion.div
+                variants={fadeIn('down', 1)}
                 initial='hidden'
                 whileInView={'show'}
-                viewport={{ once: true, amount: 0}}
-                className='fixed top-10 left-10 grid grid-cols-1 text-left leading-tight'>
-                    <h1 className="text-[#e9e9e9] font-apercu text-5xl font-normal tracking-wide">alice.</h1>
-                    <motion.div
-                        variants={fadeIn('up', 2)}
-                        initial='hidden'
-                        whileInView={'show'}
-                        viewport={{ once: true, amount: 0}}>
-                        <h3 className="text-[#e9e9e9] tracking-wide font-apercu text-xs font-extralight">Portfolio coming soon</h3>
-                    </motion.div>
-            </motion.div>
-        <h1 className="fixed top-40 left-200 tracking-wide font-apercu text-[#222]">Enter with {correctCombination.join('')}</h1>
-        <button className="fixed mt-4 px-4 py-2 top-10 right-2O tracking-wide font-thin font-apercu text-[#222] text-sm transition duration-700 ease-in-out hover:text-white">Not that simple, but remember, <br />your mouse/cursor knows everything.</button>
+                viewport={{ once: true, amount: 0}}>
         <div className="flex justify-center">
           {combination.map((digit, index) => (
             <div key={index} className="w-16 h-16 m-2 text-[#e9e9e9] flex justify-center items-center text-xl cursor-pointer" onClick={() => handleClick(index)}>
@@ -50,9 +45,16 @@ export default function EnterCode() {
             </div>
           ))}
         </div>
+        </motion.div>
+        <motion.div
+                variants={fadeIn('up', 2)}
+                initial='hidden'
+                whileInView={'show'}
+                viewport={{ once: true, amount: 0}}>
         <button className="mt-4 px-4 py-2 text-white tracking-wide font-apercu font-thin text-sm" onClick={checkCombination}>
           {codeCracked ? 'Code cracked!' : 'Crack the code.'}
           <br /><span className="px-4 py-2 text-[#222] tracking-wide font-apercu font-thin text-sm transition duration-700 ease-in-out hover:text-white">{codeCracked ? 'Portfolio is coming soon' : 'Your mouse/cursor could help.'}</span></button>
+          </motion.div>
       </div>
     </div>
   );
