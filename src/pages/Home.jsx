05b22c8c2c23886481dba3ab73de21013fa66983
alice bergonhe <alicebergonhe@macbook-air-de-alice.home>
2024-05-03 14:23:20 +0200

    add Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+      <div className="h-[800px] w-full">
+        <img src="./images/landing.jpg" alt="Space Oddity reference" className="w-full h-full object-cover" />
+      </div>
+    );
+  }
+  
+  export default Home;
