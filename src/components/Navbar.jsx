0ab86d56cf4dc618cb302d972f16c68f03aedfb3
alice bergonhe <alicebergonhe@macbook-air-de-alice.home>
2024-05-03 15:29:34 +0200

    update Navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,14 @@
 function Navbar() {
     return (
       <div className="h-[80px] w-full fixed">
-        <section className="flex justify-between p-10">
+        <section className="flex justify-between p-10 mix-blend-exclusion text-[#222]">
             <div className="flex-row text-2xl">
                 <h1>Alice Bergonhe</h1>
-                <a className="underline underline-offset-8" href="https://www.linkedin.com/in/alicebergonhe/" 
-                target="_blank" rel="noopener noreferrer">Frontend developer</a>
+                <a className="flex items-center" href="https://www.linkedin.com/in/alicebergonhe/" 
+                target="_blank" rel="noopener noreferrer">Frontend developer <span className="-mb-3 ml-1" ><ion-icon  name="arrow-forward-outline"></ion-icon></span></a>
             </div>
-            <div className="flex gap-8">
-                <h1>about</h1>
-                <h1>works</h1>
-                <h1>contact</h1>
+            <div className="flex text-4xl">
+                <ion-icon name="add-outline"></ion-icon>
             </div>
         </section>
       </div>
