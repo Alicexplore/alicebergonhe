24872a8ad7916dfb7b5bc47183282a8b73d8ac26
alice bergonhe <alicebergonhe@air-de-alice.home>
2024-06-19 17:04:06 +0200

    add preloader page

---
+++ src/pages/Preloader.jsx
@@ -0,0 +1,99 @@
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../fadeIn';
+
+const Preloader = () => {
+  const [animationComplete, setAnimationComplete] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.add('overflow-hidden');
+
+    setTimeout(() => {
+      setAnimationComplete(true);
+      document.body.classList.remove('overflow-hidden');
+    }, 4000);
+  }, []);
+
+  return (
+    <div className='fixed w-full h-full z-50'>
+      {!animationComplete && (
+        <div className='w-full h-full bg-[#f2f2f2] flex items-center justify-center text-[#222]'>
+          <div className='bg-[#222] h-2 w-full max-w-md rotate-180'>
+            <motion.div
+              className='h-2 bg-[#f2f2f2]'
+              initial={{ width: '100%' }}
+              animate={{ width: '0%' }}
+              transition={{ duration: 3 }}
+            />
+          </div>
+        </div>
+      )}
+      {animationComplete && (
+        <>
+          <div className='fixed top-0 left-0 w-full h-full flex'>
+            <motion.div
+              initial={{ y: 0 }}
+              animate={{ y: 1000 }}
+              transition={{ duration: 0.8 }}
+              className='w-1/4 h-full bg-white'
+            />
+            <motion.div
+              initial={{ y: 0 }}
+              animate={{ y: 1000 }}
+              transition={{ duration: 0.8, delay: 0.5 }}
+              className='w-1/4 h-full bg-white'
+            />
+            <motion.div
+              initial={{ y: 0 }}
+              animate={{ y: 1000 }}
+              transition={{ duration: 0.8, delay: 1 }}
+              className='w-1/4 h-full bg-white'
+            />
+            <motion.div
+              initial={{ y: 0 }}
+              animate={{ y: 1000 }}
+              transition={{ duration: 0.8, delay: 1.5 }}
+              className='w-1/4 h-full bg-white'
+            />
+          </div>
+        </>
+      )}
+      {!animationComplete && (
+        <div className='fixed inset-0 flex items-center justify-center p-8 text-[#222] z-50'>
+          <motion.div
+            variants={fadeIn('down', 0.5)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: true, amount: 0}}
+            className='text-2xl'>
+                <h1>Alice Bergonhe</h1>
+          </motion.div>
+          <motion.div
+            variants={fadeIn('down', 0.9)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: true, amount: 0}}
+            className='text-2xl'>
+                <h1>Frontend Developer</h1>
+          </motion.div>
+          <motion.div
+            variants={fadeIn('down', 1.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: true, amount: 0}}
+            className='text-2xl'>
+                <h1>Folio 2024</h1>
+          </motion.div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Preloader;
+
+
+
+
+
+
