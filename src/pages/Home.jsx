b5efa8571e86b2f2e148c9d055dd597a1d4fdad0
alice bergonhe <alicebergonhe@air-de-alice.home>
2024-06-26 17:17:37 +0200

    rename Hello page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,52 @@
-function Home() {
-    return (
-      <div className="h-[800px] w-full bg-[#222] flex items-center justify-center">
-        <img src="./images/initials.svg" alt="Space Oddity reference" className="relative" width={1000}/>
-      </div>
-    );
-  }
-  
-  export default Home;
+import { motion } from "framer-motion";
+import ScrollDown from '../components/ScrollDown';
+
+const Home = () => {
+  return (
+  <div className="relative w-full h-screen flex flex-col items-center justify-center overflow-hidden">
+      <div className="absolute inset-0 bg-[#222] z-0"></div>
+    <motion.img
+      src="./images/clouds1.png"
+      initial={{ y: "800px", opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ duration: 4, delay: 0 }}
+      viewport={{ once: false }}
+      className="absolute w-[300%] left-[-500px] bottom-[-210px] sm:left-[-360px] sm:bottom-[-300px] sm:w-[110%] h-auto z-0 max-w-none"
+    />
+    <motion.img
+      src="./images/bird.png"
+      initial={{ y: "800px", opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ duration: 6, delay: 0 }}
+      viewport={{ once: false }}
+      className="absolute hidden sm:block sm:left-[-290px] sm:bottom-[-30px] sm:w-[110%] h-auto z-20 max-w-none"
+    />
+     <motion.img
+        src="./images/lightning-A.svg"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: [0, 1, 0] }}
+        transition={{ times: [0, 0.1, 0.3, 0.4, 0.6], duration: 1, delay: 5, repeat: Infinity, repeatDelay: 13 }}
+        className="absolute hidden sm:block left-10 top-[-800px] w-[850px] h-auto z-10 max-w-none"
+      />
+      <motion.img
+        src="./images/lightning-C.svg"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: [0, 1, 0] }}
+        transition={{ times: [0, 0.1, 0.3, 0.4, 0.6], duration: 1, delay: 8.5, repeat: Infinity, repeatDelay: 13 }}
+        className="absolute hidden sm:block right-[-120px] top-[-950px] w-[850px] h-auto z-10 max-w-none"
+      />
+      <motion.img
+        src="./images/lightning-D.svg"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: [0, 1, 0] }}
+        transition={{ times: [0, 0.1, 0.3, 0.4, 0.6], duration: 1, delay: 9, repeat: Infinity, repeatDelay: 15 }}
+        className="absolute left-10 top-[-800px] w-[850px] h-auto z-10 max-w-none"
+      />
+      <section className="z-20 flex flex-col items-center justify-center mix-blend-exclusion">
+        <ScrollDown />
+      </section>
+    </div>
+  );
+};
+
+export default Home;
