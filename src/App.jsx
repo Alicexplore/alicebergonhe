dec808a8ec5553b360678203bc7181436eab72a6
alice bergonhe <alicebergonhe@air-de-alice.home>
2024-06-26 17:12:27 +0200

    update new architecture app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import About from './pages/About'
+import './App.css';
+import './index.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import White from './pages/White';
+import Contact from './pages/Contact';
+import Sneakers from './pages/Sneakers';
+import LeftNavbar from './components/LeftNavbar';
+import Menu from './components/Menu';
+import CustomCursor from './components/CustomCursor';
 
-function App() {
 
+export default function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <About />
-    </>
-  )
+    <div>
+      <div className="snap-y snap-mandatory overflow-y-scroll h-screen flex-grow z-0">
+          <CustomCursor />
+          <LeftNavbar />
+          <Menu />
+          <div className="snap-always snap-start">
+              <Home />
+          </div>
+          <div className="snap-always snap-start">
+              <About />
+          </div>
+          <div className="snap-always snap-start">
+              <White />
+          </div>
+          <div className="snap-always snap-start">
+              <Sneakers />
+          </div>
+          <div className="snap-always snap-start">
+              <Sneakers />
+          </div>
+          <div className="snap-always snap-start">
+              <Sneakers />
+          </div>
+          <div className="snap-always snap-start"> 
+              <Contact />
+          </div>
+      </div>
+    </div>
+  );
 }
-
-export default App
