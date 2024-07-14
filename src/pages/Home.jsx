bf8eac0a623240acf3ee2d91941bdf614722a305
alice bergonhe <alicebergonhe@air-de-alice.home>
2024-07-14 15:58:43 +0200

    push waiting version app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -147,14 +147,6 @@ const Home = () => {
           className="absolute hidden sm:block sm:left-[-200px] sm:top-[-140px] sm:w-[1100px] h-auto z-0 max-w-none"
         />
         </div>
-        {/* <motion.img
-          src="./images/nuage.svg"
-          initial={{ y: "0", opacity: 0 }}
-          whileInView={{ y: 0, opacity: 1 }}
-          transition={{ duration: 3, delay: 1.2 }}
-          viewport={{ once: false }}
-          className="absolute right-0 bottom-0 w-[20%] z-0 max-w-none"
-        /> */}
       <div className="fixed z-50 w-full">
       <motion.div
             variants={fadeIn('down', 0.5)}
@@ -164,16 +156,22 @@ const Home = () => {
             >
         <div className="flex justify-between p-8 w-full">
           <div className="flex flex-col text-md">
-            <svg
+          <a
+              className="hover:underline gap-1 flex items-center justify-center"
+              target="_blank"
+              href="https://www.linkedin.com/in/alicebergonhe/ "
+            >
+               <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 992.13 992.13"
               fill="currentColor"
-              className={`w-[70px] transition-colors duration-1000 ${text}`}>
+              className={`w-[70px] transition-colors duration-1000 ${hoverText} ${text} ${border}`}>
               <path
                 d="M483.61,613.09L278.67,146.39h0l103.19-.03c153.49-2.85,293.4,87.58,353.84,228.7l204.97,467.04h0l-113.76-.02c-149.7-1.55-284.36-91.37-343.31-228.99Z"
               ></path>
               <circle cx="251.34" cy="648.65" r="193.9"></circle>
-            </svg>
+            </svg>             
+            </a>
           </div>
           <div className="hidden md:flex md:flex-col items-center justify-center text-center ml-10">
             <h1 className="text-xs">
@@ -221,7 +219,7 @@ const Home = () => {
           <h1 className="hidden md:block text-xs font-normal">CREATIVE FRONTEND</h1>
         </motion.div>
         </div>
-        <div className="col-span-1 hidden md:flex justify-end">
+        <div className="col-span-1 hidden md:flex justify-end z-50">
         <motion.div
             variants={fadeIn('', 2)}
             initial='hidden'
@@ -288,29 +286,6 @@ const Home = () => {
             <h2 className="text-sm font-normal leading-10">(CREATIVE FRONTEND DEVELOPER)</h2>
             </motion.div> */}
           
-         
-
-           <div className="absolute flex flex-col items-center justify-center text-center md:hidden text-sm font-normal mt-10 w-full">
-          <motion.div
-            variants={fadeIn('up', 2)}
-            initial='hidden'
-            whileInView={'show'}
-            viewport={{ once: true, amount: 0}}
-            >
-            <div className='flex mt-2 items-center justify-center'>
-            <a
-              className="hover:underline gap-1 flex items-center justify-center"
-              target="_blank"
-              href="https://www.linkedin.com/in/alicebergonhe/ "
-            >
-               LINKEDIN
-              <span className="text-md mt-1 -rotate-45">
-                <ion-icon name="arrow-forward-outline"></ion-icon>
-              </span>
-            </a>
-            </div>
-            </motion.div>
-          </div> 
         </div>
         <div className="col-span-1 hidden md:flex justify-start z-50">
         <motion.div
@@ -324,7 +299,7 @@ const Home = () => {
             viewBox="0 0 106.18 131.06"
             width="20"
             fill="currentColor"
-            className={`transition-colors duration-1000 ${text}`}>
+            className={`transition-colors duration-1000 z-50 ${text}`}>
             <path
               className="cls-1"
               d="M104.1,65.8c-27.47-1.44-49.57-28.7-50.99-62.78-1.42,34.14-23.6,61.43-51.14,62.78v.13c27.54,1.35,49.72,28.65,51.14,62.78,1.42-34.07,23.52-61.34,50.99-62.78v-.14Z"
